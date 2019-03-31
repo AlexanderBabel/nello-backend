fc48cc4ab8192453fc073dc71afd2cc15190b30d
Alexander Babel <alexander.babel@icloud.com>
2019-03-31 22:21:48 +0200

    Added eslint Merged ports

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node
+
+# Create app directory
+WORKDIR /opt/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+
+RUN npm ci --only=production
+# If you are building your code for production
+# RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
+
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -1,59 +1,58 @@
-try {
-    require('./env');
-} catch (err) {
-    console.log('Could not load env.js');
-    process.exit();
-}
-
+require('dotenv/config');
 const app = require('express')();
 const bodyParser = require('body-parser');
-const io = require('socket.io')(process.env.SOCKET_PORT);
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const Chance = require('chance');
 
 const chance = new Chance();
 const clients = [];
 
+function generateRandomWebhookName() {
+  return `${chance
+    .sentence({ words: 5 })
+    .replace(/ /g, '-')
+    .toLowerCase()
+    .replace(/\./g, '')}-${chance.integer({ min: 0, max: 9999 })}`;
+}
+
 app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    for (const client of clients) {
-        if (`/callback/${client.name}` === req.path) {
-            client.socket.emit('call', req.body);
-            res.status(200).send('OK');
-            return;
-        }
+app.use((req, res) => {
+  const client = clients.find(c => `/callback/${c.name}` === req.path);
+  if (!client) {
+    res.status(404).send('Not Found');
+    return;
+  }
+
+  client.socket.emit('call', req.body);
+  res.status(200).send('OK');
+});
+
+io.on('connection', socket => {
+  socket.on('getWebhook', () => {
+    const name = generateRandomWebhookName();
+    clients.push({
+      socket,
+      name
+    });
+
+    socket.emit('webhook', {
+      url: `${process.env.HOST_NAME}/callback/${name}`
+    });
+  });
+
+  socket.on('disconnect', () => {
+    let index = -1;
+    for (let i = 0; i < clients.length; i += 1) {
+      if (clients[i].socket === socket) {
+        index = i;
+      }
     }
 
-    res.status(404).send('Not Found');
-});
-app.listen(process.env.REST_PORT);
-
-io.on('connection', (socket) => {
-    socket.on('getWebhook', (data) => {
-        const name = generateRandomWebhookName();
-        clients.push({
-            socket,
-            name,
-        });
-
-        socket.emit('webhook', {
-            url: `${process.env.HOST_NAME}/callback/${name}`
-        });
-    });
-
-    socket.on('disconnect', () => {
-        let index = -1;
-        for (let i = 0; i < clients.length; i++) {
-            if (clients[i].socket === socket) {
-                index = i;
-            }
-        }
-
-        if (index !== -1) {
-            clients.splice(index, 1);
-        }
-    });
+    if (index !== -1) {
+      clients.splice(index, 1);
+    }
+  });
 });
 
-function generateRandomWebhookName() {
-    return `${chance.sentence({ words: 5 }).replace(/ /g, '-').toLowerCase().replace(/\./g, '')}-${chance.integer({ min: 0, max: 9999 })}`;
-}
+server.listen(process.env.PORT);
